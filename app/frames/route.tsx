f19883abbfbf3afdbbe61addd1816882aebf3a50
David Correa <david.correav@gmail.com>
2024-06-30 18:57:44 -0500

    Fixes and qc

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -39,75 +39,82 @@ const returnTrendingTokens = async () => {
 
 const frameHandler = frames(
   async (ctx) => {
+    init(process.env.NEXT_PUBLIC_AIRSTACK_API_KEY as string);
+    console.log("ctx", ctx.message);
+    console.log("ctx.FID", ctx.message?.requesterFid);
     let autoAction = false;
     let emberResponse = "No response from Ember";
 
-    const response: any = await returnTrendingTokens();
+    const tokenResponse: any = await returnTrendingTokens();
 
-    console.log(response);
+    console.log(tokenResponse);
 
-    const counter = ctx.message
-      ? ctx.searchParams.op === "+"
-        ? ctx.state.counter + 1
-        : ctx.state.counter - 1
-      : ctx.state.counter;
-
-    if (ctx.message?.inputText) {
-      const response: any = await fetchEmberResponse(ctx.message.inputText);
-      emberResponse = response.inputText as string;
-      console.log(emberResponse);
-    }
     if (ctx.searchParams.op === "SEND") {
       autoAction = true;
-      const response: any = await fetchEmberResponse("TEXT QUERY FOR SEND");
+      const response: any = await fetchEmberResponse("send token");
       emberResponse = response.inputText as string;
       console.log(emberResponse);
     }
 
     if (ctx.searchParams.op === "SWAP") {
       autoAction = true;
-      const response: any = await fetchEmberResponse("TEXT QUERY FOR SWAP");
+      const response: any = await fetchEmberResponse("swap token on Base");
       emberResponse = response.inputText as string;
       console.log(emberResponse);
     }
 
     if (ctx.searchParams.op === "BUY") {
       autoAction = true;
-      const response: any = await fetchEmberResponse("TEXT QUERY FOR BUY");
+      const response: any = await fetchEmberResponse(
+        `buy ${tokenResponse[0]?.symbol} with address ${tokenResponse[0]?.address} on Base`
+      );
       emberResponse = response.inputText as string;
       console.log(emberResponse);
     }
 
+    if (ctx.searchParams.op === "MSG") {
+      autoAction = true;
+      const response: any = await fetchEmberResponse(ctx.message?.inputText);
+      emberResponse = response.inputText as string;
+      console.log(emberResponse);
+    }
+
+    const stringLabel = "Buy $" + tokenResponse[0]?.symbol;
+
     const ButtonsArray = [
-      !ctx.message?.inputText && !autoAction && (
-        <Button action="post" target={{ pathname: "/", query: { op: "SEND" } }}>
-          Send
-        </Button>
-      ),
       !ctx.message?.inputText && !autoAction && (
         <Button action="post" target={{ pathname: "/", query: { op: "SWAP" } }}>
           Swap Token
         </Button>
       ),
       !ctx.message?.inputText && !autoAction && (
-        <Button action="post" target={{ pathname: "/", query: { op: "BUY" } }}>
-          Buy Token
+        <Button action="post" target={{ pathname: "/", query: { op: "SEND" } }}>
+          Send Token
         </Button>
       ),
-      <Button action="post" target={{ pathname: "/", query: { op: "" } }}>
+      !ctx.message?.inputText && !autoAction && (
+        <Button
+          action="post"
+          target={{ pathname: "/", query: { op: "BUY" } }}
+          children={stringLabel}
+        />
+      ),
+      <Button action="post" target={{ pathname: "/", query: { op: "MSG" } }}>
         Message
       </Button>,
     ];
     return {
       image: (
         <div tw="flex flex-col bg-orange-300 w-full h-full justify-center items-center">
-          {response?.length > 0 && (
-            <div tw="flex"> Trending Token: {response[0]?.symbol}</div>
+          {tokenResponse?.length > 0 && (
+            <div tw="flex"> Trending Token: {tokenResponse[0]?.symbol}</div>
           )}
           {!ctx.message?.inputText && (
             <div tw="flex flex-col">
-              <div>FID: {ctx.userDetails?.fid}</div>
-              {ctx.userDetails?.profileName} Chat to Ember!{" "}
+              {(ctx.userDetails?.profileName && ctx.userDetails?.profileName) ||
+                (ctx.userDetails?.fnames.length > 0 &&
+                  ctx.userDetails?.fnames[0])}{" "}
+              Chat to Ember!{" "}
             </div>
           )}
           {(ctx.message?.inputText || autoAction) && (
@@ -117,7 +124,6 @@ const frameHandler = frames(
       ),
       textInput: "Say something",
       buttons: ButtonsArray,
-      state: { counter: counter },
     };
   },
   {
