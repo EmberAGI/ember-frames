a28c2c88c365920a15683806f0bb3627c8e8a16e
Tom Daniel <0xtomdaniel@gmail.com>
2024-06-30 23:37:28 -0400

    Integrated Ember API

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -12,8 +12,83 @@ import {
   TimeFrame,
 } from "@airstack/frames";
 import { init } from "@airstack/frames";
+import z from "zod";
 
-const fetchEmberResponse = async (inputText: string | undefined) => {
+const ChatEmberRespons = z.object({
+  status: z.union([
+    z.literal("done"),
+    z.literal("processing"),
+    z.literal("error"),
+  ]),
+  message: z.string(),
+  sign_tx_url: z.string().optional(),
+});
+
+const fetchEmberResponse = async (inputText: string | undefined, fid: string | undefined, username?: string) => {
+  const response = await fetch(`https://devapi.emberai.xyz/v1/chat`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "authorization": `Bearer ${process.env.EMBER_API_KEY as string}`
+    },
+    body: JSON.stringify({
+      user_id: fid,
+      message: inputText,
+      username,
+    }),
+  });
+
+  if (!response.ok || response.body == null) {
+    throw new Error("Failed to connect to Ember server");
+  }
+
+  const reader = response.body.getReader();
+  const decoder = new TextDecoder();
+  while (true) {
+    const { done, value } = await reader.read();
+    const { event, rawData } = parseSseResponse(decoder.decode(value));
+    if (done && event !== "done") {
+      throw new Error("Invalid response");
+    }
+
+    if (rawData == null) {
+      continue;
+    }
+
+    const data = await ChatEmberRespons.safeParseAsync(JSON.parse(rawData));
+    if (!data.success) {
+      throw new Error("Invalid response");
+    }
+    const response = data.data;
+
+    switch (event) {
+      case "done":
+        return response;
+      case "activity":
+        continue;
+      case "error":
+        return `Error: ${response.message}`;
+      default:
+        throw new Error("Invalid response");
+    }
+  }
+}
+
+function parseSseResponse(value: string) {
+  const lines = value.split("\n");
+  let event = undefined;
+  let rawData = undefined;
+
+  for (const line of lines) {
+    if (line.startsWith("event: ")) {
+      event = line.substring("event: ".length).trim();
+    } else if (line.startsWith("data: ")) {
+      rawData = line.substring("data: ".length);
+    }
+  }
+
+  return { event, rawData };
+
   //mocking the async response using Timeout
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -51,37 +126,41 @@ const frameHandler = frames(
 
     console.log(tokenResponse);
 
+    const fid_string = String(ctx.message?.requesterFid);
+
     if (ctx.searchParams.op === "SEND") {
       autoAction = true;
-      const response: any = await fetchEmberResponse("send token");
+      const response: any = await fetchEmberResponse("send token", fid_string, ctx.userDetails?.profileName);
       response.sign_tx_url && (signTxn = response.sign_tx_url);
-      emberResponse = response.inputText as string;
+      emberResponse = response.message as string;
       console.log(emberResponse);
     }
 
     if (ctx.searchParams.op === "SWAP") {
       autoAction = true;
-      const response: any = await fetchEmberResponse("swap token on Base");
+      const response: any = await fetchEmberResponse("swap token on Base", fid_string, ctx.userDetails?.profileName);
       response.sign_tx_url && (signTxn = response.sign_tx_url);
-      emberResponse = response.inputText as string;
+      emberResponse = response.message as string;
       console.log(emberResponse);
     }
 
     if (ctx.searchParams.op === "BUY") {
       autoAction = true;
       const response: any = await fetchEmberResponse(
-        `buy ${tokenResponse[0]?.symbol} with address ${tokenResponse[0]?.address} on Base`
+        `buy ${tokenResponse[0]?.symbol} with address ${tokenResponse[0]?.address} on Base`,
+        fid_string,
+        ctx.userDetails?.profileName
       );
       response.sign_tx_url && (signTxn = response.sign_tx_url);
-      emberResponse = response.inputText as string;
+      emberResponse = response.message as string;
       console.log(emberResponse);
     }
 
     if (ctx.searchParams.op === "MSG") {
       autoAction = true;
-      const response: any = await fetchEmberResponse(ctx.message?.inputText);
+      const response: any = await fetchEmberResponse(ctx.message?.inputText, fid_string, ctx.userDetails?.profileName);
       response.sign_tx_url && (signTxn = response.sign_tx_url);
-      emberResponse = response.inputText as string;
+      emberResponse = response.message as string;
       console.log(emberResponse);
     }
 
